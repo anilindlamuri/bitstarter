efcb63eef7345f0427c7c911587a20bd521d4edb
anilindlamuri <anilsjunk@gmail.com>
2013-07-10 04:30:13 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  response.send(data.toString());
+});
+
+  //response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
